2ba1a1c42d137ecea1ac6da3456292befeb220ab
AS <gerato4887@gotemv.com>
2025-06-19 18:58:55 +0200

    Routes Pages,Side bar,and Overview page created(30%)

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import { BarChart2, ShoppingBag, User, Zap } from "lucide-react";
+const OverviewPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-1">
+      <Header title="Overview" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/*STATS*/}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <StatCard
+            name="Total Sales"
+            icon={Zap}
+            value="R120,345"
+            color="#6366F1"
+          />
+          <StatCard name="New User" icon={User} value="1,234" color="#8B5CF6" />
+          <StatCard
+            name="Total Products"
+            icon={ShoppingBag}
+            value="567"
+            color="#EC4899"
+          />
+          <StatCard
+            name="Conversion Rate"
+            icon={BarChart2}
+            value="12.5%"
+            color="#10B981"
+          />
+        </motion.div>
+      </main>
+    </div>
+  );
+};
+
+export default OverviewPage;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/common/Header";
+
+const ProductsPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-1">
+      <Header title="Products" />
+    </div>
+  );
+};
+
+export default ProductsPage;
